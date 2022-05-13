d4fc4f0ccc2e102ebbe4f663556d21739013c327
Sphelgay <105509308+Sphelgay@users.noreply.github.com>
2022-05-13 22:20:32 +0600

    doing 1st commit uploading basic java code

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd
+{
+public static void main(String[] args)
+{
+int no=63;
+if(no%2==0)
+System.out.println("No is Even");
+else
+if(no%2!=0)
+System.out.println("No is Odd");
+}
+}
+
+
